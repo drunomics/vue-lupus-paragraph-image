e4373572c2bcf50743fa57a381e140cf8748f887
Arthur Lorenz <arthur@drunomics.com>
2018-06-25 17:16:38 +0200

    Removed unnecessary type property and unused slot.

--- src/pg-image.vue
+++ src/pg-image.vue
@@ -1,21 +1,17 @@
 <template>
-  <div :class="paragraphClass">
-    <slot
-      v-if="isImage"
-      name="field_image">
-      <div class="paragraph-image__wrapper">
-        <img
-          :src="dataImgSrc"
-          class="paragraph-image__image"
-        >
-        <div class="paragraph-image__copyright">
-          {{ fieldCopyright }}
-        </div>
-        <div class="paragraph-image__caption">
-          {{ fieldCaption }}
-        </div>
+  <div class="paragraph paragraph--image">
+    <div class="paragraph-image__wrapper">
+      <img
+        :src="dataImgSrc"
+        class="paragraph-image__image"
+      >
+      <div class="paragraph-image__copyright">
+        {{ fieldCopyright }}
       </div>
-    </slot>
+      <div class="paragraph-image__caption">
+        {{ fieldCaption }}
+      </div>
+    </div>
     <slot/>
   </div>
 </template>
@@ -23,18 +19,9 @@
   export default {
     name: 'PgImage',
     props: {
-      type: { type: String, default: () => 'image' },
       dataImgSrc: { type: String, default: () => '' },
       fieldCopyright: { type: String, default: () => '' },
       fieldCaption: { type: String, default: () => '' },
-    },
-    computed: {
-      isImage() {
-        return this.type === 'image';
-      },
-      paragraphClass() {
-        return `paragraph paragraph--${this.type}`;
-      },
     },
   };
 </script>
